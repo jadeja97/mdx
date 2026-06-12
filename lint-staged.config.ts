4e908b3f73c8ceb2da02cfd0e2f14dabd3ec0747
Pradipsinh Jadeja <224366804+jadeja97@users.noreply.github.com>
2026-06-12 22:51:34 +0530

    fix: lint-staged

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,8 +1,15 @@
 const lintStagedConfig = {
-  // function here prevents file names appended to the script
-  "*": () => {
-    return "pnpm check";
+  // 1. run type checking once for the whole project
+  // using a function to prevent file arguments being passed to tsc
+  "*.{ts,tsx}": () => {
+    return "tsc";
   },
+
+  // 2. run formatting and linting ONLY on the changed files
+  "*.{ts,tsx,json,md,css}": [
+    "oxfmt --check --disable-nested-config",
+    "oxlint --disable-nested-config",
+  ],
 };
 
 export default lintStagedConfig;
